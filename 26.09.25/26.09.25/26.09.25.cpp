b8bb9770d3440d5f8e25e1c7575c74a7be0f52a4
GoodMan <2211doge1122@gmail.com>
2025-09-26 09:34:13 +0300

    Добавил задачу

---
+++ 26.09.25/26.09.25/26.09.25.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    float L; 
+    int r2; int r1; int x0; int y0; int x; int y;
+
+    cout << "Введите две координаты(x0, y0) для центры окружности: "; cin >> x0 >> y0;
+    cout << "Введите радиус кольца(r1, r2): "; cin >> r1 >> r2;
+    cout << "Введите координаты точки(x, y): "; cin >> x >> y;
+    
+    L = sqrt(pow(x - x0, 2) + pow(y - y0, 2));
+    
+    if (L <= r2 && L >= r1) {
+        cout << "Точка L прнинадлежит кольцу";
+    }
+    else {
+        cout << "Точка L не прнинадлежит кольцу";
+    }
+
+    return 0;
+}
+
