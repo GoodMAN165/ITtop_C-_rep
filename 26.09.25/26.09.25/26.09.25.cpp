b5acc41e198afd117a2e12b2c68a42b20ba5663f
GoodMan <2211doge1122@gmail.com>
2025-09-26 11:01:28 +0300

    Решил задачки 26.09

--- 26.09.25/26.09.25/26.09.25.cpp
+++ 26.09.25/26.09.25/26.09.25.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "RU");
-
+    /*
     float L; 
     int r2; int r1; int x0; int y0; int x; int y;
 
@@ -22,7 +22,43 @@ int main()
     else {
         cout << "Точка L не прнинадлежит кольцу";
     }
+    */
 
-    return 0;
+    //Задача 1 "Допуск к экзамену"
+    int gr1; int gr2; int gr3; int gr4; int gr5;
+    
+    cout << "Введите 5 оценок для студента: "; cin >> gr1 >> gr2 >> gr3 >> gr4 >> gr5;  
+    
+    int res =(gr1 + gr2 + gr3 + gr4+ gr5) / 5;
+    
+    cout << "Средний балл студента: " << res << "\n";
+    
+    switch (res) {
+    case 4:
+    case 5:
+        cout << "Студент допущен\n";
+        break;
+    default:
+        cout << "Студент не допущен\n";
+        break;
+    }
+    
+    //Задача 2 "Чёт/не чёт"
+    int number;
+    double Dnumber;
+    cout << "Введите число: "; cin >> number;
+
+    switch (number%2) {
+    case 0:
+        number *= 3;
+        cout << "Число чётное, оно будет умножено на 3" << number << "\n";
+        break;
+    default:
+        Dnumber = number / 2.0f;
+        cout << "Число не чётное, оно будет поделено на 2: " << Dnumber << "\n";
+        break;
+    }
+
+return 0;
 }
 
